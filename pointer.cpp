8cf6e018739af595cc9da20cb707361fe1c2559b
Anguwala Akshay Raj Singh <67822687+AAkshayRajSingh@users.noreply.github.com>
2022-02-23 19:49:28 +0530

    Create pointer.cpp

---
+++ pointer.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=5;
+    cout<<num<<endl;
+//address of Operator-&
+
+cout<<"address of num is "<<&num<<endl;
+
+int *ptr=&num;
+
+cout<<"value is : "<<ptr<<endl;
+cout<<"value is : "<<*ptr<<endl;
+    
+    double d=4.3;
+    double*p2=&d;
+    
+cout<<"value is : "<<p2<<endl;
+cout<<"value is : "<<*p2<<endl;
+
+
+cout<<"size of integer is  : "<<sizeof(num)<<endl;
+cout<<"size of pointer is : "<<sizeof(ptr)<<endl;
+    return 0;
+
+}
